10aa8fb9411de8b961e68df7e618fc54c7c8576c
Aidan "Silver0006" Marias <Silver0006@kitsunehouse.media>
2026-04-27 13:10:41 -0400

    Refactored from independent functions to class. Changed from string concat to a struct.
    - Threading would require the tokenize() to be able to handle multiple
      instances, however, the helper functions of tokenize need to be able
      to access its variables. I don't like having a large number of
      parameters for my functions. Instead, I created a tokenizer class
      which holds the variables. Now all tokenzier function can access
      their need variables in their own instance. This would mean that to
      use tokenize() you'd need to create an object, so I wrote a simple
      tokenize() that creates the instance and returns the output. For
      threading all I should need to do is add the ability to pass a line
      number.
    - Switched from handling tokens only through strings to a token struct.
      This will allow direct passing of tokens to the parser if I get to
      that point.

--- cpp/tokenizer.cpp
+++ cpp/tokenizer.cpp
@@ -2,149 +2,166 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include <vector>
 #include "helper.hpp" // Generalized helper functions
 #include "tokenizer.hpp"
 
+// Global Variables
+const std::string section = "Tokenizer";
 static const std::unordered_map<std::string, std::string> patterns = {
     {}, 
 };
 
-// Global Variables
-int tokenStart = 1, charCount = 1, lineCount = 1;
-std::string tokens, token;
-const std::string section = "Tokenizer";
+struct token {
+    std::string generate()  {
+        // if valueExists return token with value
+        return value.empty() ? "{'tag': '" + tag + "', 'line': " 
+                + std::to_string(lineCount) + ", 'column': " 
+                + std::to_string(tokenStart) + "}" 
+                : "{'tag': '" + tag + "', 'value': " + value + ", 'line': " 
+                + std::to_string(lineCount) + ", 'column': " 
+                + std::to_string(tokenStart) + "}";
+    }
+    int tokenStart = 1, charCount = 1, lineCount = 1;
+    std::string tag, value;
+};
 
-// Helper functions
-std::string genToken(const std::string tag, const bool valueExists=false) {
-    // if valueExists return token with value
-    return valueExists ? "{'tag': '" + tag + "', 'value': " + token + ", 'line': " 
-            + std::to_string(lineCount) + ", 'column': " 
-            + std::to_string(tokenStart) + "}, " 
-            : "{'tag': '" + tag + "', 'line': " 
-            + std::to_string(lineCount) + ", 'column': " 
-            + std::to_string(tokenStart) + "}, ";
-}
-
-void strTokenize(const std::string &source_code, std::string::iterator &pos) {
-    token.clear();
-    bool stringEmpty = true;
-    tokenStart = charCount;
-    ++charCount;
-    ++pos;
-    token += '\'';
-    while (*pos != '"') {
-        if (pos == source_code.cend()) {
-            trivialError("Tokenizer", "missing \"", lineCount, charCount);
-        }
-        stringEmpty = false;
+class tokenizer {
+    void strTokenize(const std::string &source_code, std::string::iterator &pos) {
+        bool stringEmpty = true;
+        currentToken->tokenStart = charCount;
         ++charCount;
-        token += *pos;     
         ++pos;
-    }
-    token += '\'';
-    ++pos;
-    if (stringEmpty) {
-        token.clear();
-    } else {
-        token = genToken("identifier", true);
-    }
-}
-
-void keywordTokenize(const std::string &source_code, std::string::iterator &pos) {
-    token.clear();
-    tokenStart = charCount;
-    switch (*pos) {
-        default:
-            while(!isspace(*pos) and pos != source_code.cend()) {
-               token += *pos;
-               ++pos;
-               ++charCount;
+        currentToken->value += '\'';
+        while (*pos != '"') {
+            if (pos == source_code.cend()) {
+                trivialError("Tokenizer", "missing \"", lineCount, charCount);
             }
-    }
-    token = genToken(token);
-}
-
-// Main tokenizer function
-std::string tokenize(std::string source_code) {
-    if (source_code.empty()) {
-        trivialError("Tokenizer", "tokenize() missing argument");
-    }
-
-    std::string::iterator pos = source_code.begin();
-    tokens = "[";
-    while (pos != source_code.cend()) {
-        switch (*pos) {
-            // space, tab, return caridge for windows
-            case ' ': case '\t': case '\r':
-                ++charCount;
-                ++pos;
-                break;
-            // new line             
-            case '\n':
-                charCount = 1;
-                ++lineCount;
-                ++pos;
-                break;
-            // strings
-            case '"':
-                strTokenize(source_code, pos);
-                break;
-            // numbers
-            case '0': case '1': case '2': case '3': case '4': 
-            case '5': case '6': case '7': case '8': case '9': {
-                token.clear();
-                tokenStart = charCount;
-                bool decimalCheck = false;
-                while (std::isdigit(*pos) or *pos == '.') {
-                    if (*pos == '.' and decimalCheck) {
-                        trivialError("Tokenizer", "multiple decimal points", 
-                                     lineCount,charCount);
-                    } else if (*pos == '.') {
-                        decimalCheck = true;
-                    }
-                    token += *pos;
-                    ++pos;
-                    ++charCount;
-                }
-                token = genToken("number", true);
-                break;
-            }
-            // Single character tokens
-            case '+': case '-': case '*': case '/': case '%': case '(': case ')':
-            case '{': case '}': case '[': case ']': case ',': case ':': case ';':
-                token.clear();
-                tokenStart= charCount;
-                token += *pos;
-                ++pos;
-                ++charCount;
-                token = genToken(token);
-                break;
-            // Multi character tokens
-            case '!': case '=': case '|': case '&': case '<': case '>':
-                token.clear();
-                tokenStart= charCount;
-                token += *pos;
-                ++pos;
-                ++charCount;
-                token = genToken(token);
-                break;
-            default:
-                #ifdef DEBUG
-                    std::cerr << "About to call keywordTokenize()" << std::endl;
-                #endif
-                keywordTokenize(source_code, pos);
+            stringEmpty = false;
+            ++charCount;
+            currentToken->value += *pos;     
+            ++pos;
         }
-        tokens.append(token);
-        token.clear();
-        #ifdef DEBUG
-            std::cerr << "[Debug][Tokenizer] end char: " << *pos
-                      << " line: "  << lineCount 
-                      << std::endl;
-        #endif
+        currentToken->value += '\'';
+        ++pos;
+        if (stringEmpty) {currentToken->value.clear();}
+        currentToken->tag = "identifier";
     }
-    tokens += "{'tag': 'None', 'line': " + std::to_string(lineCount)
-            + ", 'column': " + std::to_string(charCount) + "}]";
-    return tokens;
-}
 
+    void keywordTokenize(const std::string &source_code, std::string::iterator &pos) {
+        currentToken->tokenStart = charCount;
+        switch (*pos) {
+            default:
+                while(!isspace(*pos) and pos != source_code.cend()) {
+                   currentToken->tag += *pos;
+                   ++pos;
+                   ++charCount;
+                }
+        }
+    }
+
+    int charCount = 1, lineCount = 1;
+    std::vector<token*> tokens;
+    token *currentToken = new token;
+
+public:
+    // Main tokenizer function
+    std::string tokenize(std::string source_code) {
+        if (source_code.empty()) {
+            trivialError("Tokenizer", "tokenize() missing argument");
+        }
+
+        std::string::iterator pos = source_code.begin();
+
+        while (pos != source_code.cend()) {
+            switch (*pos) {
+                // space, tab, return caridge for windows
+                case ' ': case '\t': case '\r':
+                    ++charCount;
+                    ++pos;
+                    break;
+                // new line             
+                case '\n':
+                    charCount = 1;
+                    ++lineCount;
+                    ++pos;
+                    break;
+                // strings
+                case '"':
+                    strTokenize(source_code, pos);
+                    break;
+                // numbers
+                case '0': case '1': case '2': case '3': case '4': 
+                case '5': case '6': case '7': case '8': case '9': {
+                    currentToken->tokenStart = charCount;
+                    bool decimalCheck = false;
+                    while (std::isdigit(*pos) or *pos == '.') {
+                        if (*pos == '.' and decimalCheck) {
+                            trivialError("Tokenizer", "multiple decimal points", 
+                                         lineCount,charCount);
+                        } else if (*pos == '.') {
+                            decimalCheck = true;
+                        }
+                        currentToken->value += *pos;
+                        ++pos;
+                        ++charCount;
+                    }
+                    currentToken->tag = "number";
+                    currentToken->charCount = charCount;
+                    break;
+                }
+                // Single character tokens
+                case '+': case '-': case '*': case '/': case '%': case '(': case ')':
+                case '{': case '}': case '[': case ']': case ',': case ':': case ';':
+                    currentToken->tokenStart = charCount;
+                    currentToken->tag += *pos;
+                    ++pos;
+                    currentToken->charCount = ++charCount;
+                    break;
+                // Multi character tokens
+                case '!': case '=': case '|': case '&': case '<': case '>':
+                    currentToken->tokenStart = charCount;
+                    currentToken->tag += *pos;
+                    ++pos;
+                    currentToken->charCount = ++charCount;
+                    break;            
+                default:
+                    #ifdef DEBUG
+                        std::cerr << "About to call keywordTokenize()" << std::endl;
+                    #endif
+                    keywordTokenize(source_code, pos);
+            }
+            tokens.push_back(currentToken);
+            currentToken = new token;
+            #ifdef DEBUG
+                std::cerr << "[Debug][Tokenizer] end char: " << *pos
+                          << " line: "  << lineCount 
+                          << std::endl;
+            #endif
+        }
+        currentToken->tag = "None";
+        currentToken->lineCount = lineCount;
+        currentToken->tokenStart = charCount;
+        tokens.push_back(currentToken);
+        currentToken = nullptr;
+        
+        // Take tokens and throw them into a string
+        std::string tokenList = "[";
+        for (size_t i = 0; i < tokens.size(); ++i){
+            i == tokens.size()-1 ?
+                tokenList += tokens[i]->generate() :
+                tokenList += tokens[i]->generate() + ", ";
+            delete(tokens[i]);
+        }
+        tokenList += "]";
+
+        return tokenList;
+    }
+};
+
+// Clean caller for tokenize
+std::string tokenize(std::string source_code) {
+    tokenizer t;
+    return t.tokenize(source_code); 
+}
 
